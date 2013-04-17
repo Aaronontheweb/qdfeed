c3607537038c9da458a184f846d7d62375f9f464
Aaron Stannard <aaron@markedup.com>
2013-04-16 22:23:32 -0700

    added the project data file to the buildscripts; going to work on creating an Albacore RAKE file

---
+++ buildscripts/project_data.rb
@@ -0,0 +1,48 @@
+#----------------------------------
+# Project data for Quick and Dirty Feed Parser
+#----------------------------------
+
+Projects = {
+	:language => "en-US",
+	:licenseUrl => "https://github.com/Aaronontheweb/qdfeed/blob/master/license.txt",
+	:projectUrl => "https://github.com/Aaronontheweb/qdfeed",
+
+	:qdfeed =>{
+		:id => "QDFeedParser",
+		:description => "A lightweight .NET library designed to give developers an agnostic way of parsing RSS 2.0 and Atom 1.0 XML syndication formats.",
+		:authors => "Aaron Stannard",
+		:dir => "QDFeedParser",
+		:test_dir => "QDFeedParser.Tests",
+		:title => "Quick and Dirty Feed Parser"
+	},
+	:qdfeed_net45 =>{
+		:id => "QDFeedParser (NET45)",
+		:description => "A lightweight .NET library designed to give developers an agnostic way of parsing RSS 2.0 and Atom 1.0 XML syndication formats (.NET 4.5 support)",
+		:authors => "Aaron Stannard",
+		:dir => "QDFeedParser (NET45)",
+		:test_dir => "QDFeedParser.Tests",
+		:title => "Quick and Dirty Feed Parser (.NET 4.5)"
+	},
+	:qdfeed_wp7 =>{
+		:id => "QDFeedParser.Mobile",
+		:description => "A lightweight .NET library designed to give developers an agnostic way of parsing RSS 2.0 and Atom 1.0 XML syndication formats (Windows Phone 7 support)",
+		:authors => "Aaron Stannard",
+		:dir => "QDFeedParser.Mobile",
+		:title => "Quick and Dirty Feed Parser (Windows Phone 7)"
+	},
+	:qdfeed_wp8 => {
+		:id => "QDFeedParser.Mobile (WP8)",
+		:description => "A lightweight .NET library designed to give developers an agnostic way of parsing RSS 2.0 and Atom 1.0 XML syndication formats (Windows Phone 8 support)",
+		:authors => "Aaron Stannard",
+		:dir => "QDFeedParser.Mobile (WP8)",
+		:title => "Quick and Dirty Feed Parser (Windows Phone 8)"
+	},
+	:qdfeed_silverlight =>{
+		:id => "QDFeedParser.Silverlight",
+		:description => "A lightweight .NET library designed to give developers an agnostic way of parsing RSS 2.0 and Atom 1.0 XML syndication formats (Silverlight 4.0 support)",
+		:authors => "Aaron Stannard",
+		:dir => "QDFeedParser.Silverlight",
+		:test_dir => "QDFeedParser.Silverlight.Tests",
+		:title => "Quick and Dirty Feed Parser (Silverlight 4.0)"
+	}
+}
