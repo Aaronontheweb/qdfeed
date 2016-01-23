cd783c45b4b8a4216ad240a039c579489cc6e60c
GuiBGP <guilhermebgp@gmail.com>
2016-01-23 12:26:20 -0200

    Changed to improve the ability to extend the code. I believe that now it is easier to create new classes to represent RSS and ATOM in a different namespace.

--- QDFeedParser/DefaultFeedInstanceProvider.cs
+++ QDFeedParser/DefaultFeedInstanceProvider.cs
@@ -2,12 +2,12 @@
 {
     internal class DefaultFeedInstanceProvider : IFeedInstanceProvider
     {
-        public Rss20Feed CreateRss20Feed(string feeduri)
+        public IFeed CreateRss20Feed(string feeduri)
         {
             return new Rss20Feed(feeduri);
         }
 
-        public Atom10Feed CreateAtom10Feed(string feeduri)
+        public IFeed CreateAtom10Feed(string feeduri)
         {
             return new Atom10Feed(feeduri);
         }
--- QDFeedParser/IFeedInstanceProvider.cs
+++ QDFeedParser/IFeedInstanceProvider.cs
@@ -2,7 +2,7 @@
 {
     public interface IFeedInstanceProvider
     {
-        Rss20Feed CreateRss20Feed(string feeduri);
-        Atom10Feed CreateAtom10Feed(string feeduri);
+        IFeed CreateRss20Feed(string feeduri);
+        IFeed CreateAtom10Feed(string feeduri);
     }
 }
