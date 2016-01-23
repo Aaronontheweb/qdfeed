b9598b530591f5e3034be7020a74adcde73874ca
GuiBGP <guilhermebgp@gmail.com>
2016-01-23 15:43:21 -0200

    Creating a Sample Podcast feed reader using ASP.NET.

---
+++ QDFeedParser.Web/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace QDFeedParser.Web.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ QDFeedParser.Web/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QDFeedParser.Web.Startup))]
+namespace QDFeedParser.Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
